c66a047a9c1444c07e8e69c69b69a07575ef3677
Hernan Fernandez <hernanjx@gmail.com>
2019-04-13 14:03:57 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:carambula/screens/home.dart';
+
+
+void main() {
+  // SystemChrome.setSystemUIOverlayStyle(
+  //   SystemUiOverlayStyle(
+  //     statusBarColor: Colors.amber,
+  //     statusBarIconBrightness: Brightness.dark,
+  //     systemNavigationBarColor: Colors.amber,
+  //     systemNavigationBarIconBrightness: Brightness.dark
+  //   )
+  // );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter tests',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.lightGreen[800],
+        accentColor: Colors.amber[600],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.deepOrange[600],
+          textTheme: ButtonTextTheme.primary
+        ),
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0),
+          title: TextStyle(fontSize: 36.0),
+          body1: TextStyle(fontSize: 12.0)
+        )
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  final _formKey = GlobalKey<FormState>();
+  final _formData = {
+    'phoneNumber': '',
+    'lastName': '',
+    'street1': '',
+    'firstName': '',
+    'street2': '',
+    'city': '',
+    'state': ''
+  };
+
+  InputDecoration _decorateTfield(String text) {
+    return InputDecoration(
+      labelText: text,
+      border: OutlineInputBorder()
+    );
+  }
+
+  void _handleForm() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print('Form data: $_formData');
+    } else {
+      print('Form not valid');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Form test"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: () => {},
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: _decorateTfield('Phone number'),
+                onSaved: (value) => _formData['phoneNumber'] = value,
+                validator: (value) {
+                  return !value.contains(RegExp('[0-9]')) ? 'Invalid phone' : null;
+                },
+              ),
+              SizedBox(height: 20),
+
+              TextFormField(
+                decoration: _decorateTfield('First name'),
+                onSaved: (value) => _formData['firstName'] = value
+              ),
+              SizedBox(height: 20),
+
+              TextFormField(
+                decoration: _decorateTfield('Last name'),
+                onSaved: (value) => _formData['lastName'] = value
+              ),
+              SizedBox(height: 20),
+
+              TextFormField(
+                decoration: _decorateTfield('Street 1'),
+                onSaved: (value) => _formData['street1'] = value
+              ),
+              SizedBox(height: 20),
+
+              TextFormField(
+                decoration: _decorateTfield('Street 2'),
+                onSaved: (value) => _formData['street2'] = value
+              ),
+              SizedBox(height: 20),
+
+              TextFormField(
+                decoration: _decorateTfield('City'),
+                onSaved: (value) => _formData['city'] = value
+              ),
+              SizedBox(height: 20),
+
+              TextFormField(
+                decoration: _decorateTfield('State'),
+                onSaved: (value) => _formData['state'] = value
+              ),
+              SizedBox(height: 20),
+
+
+              RaisedButton(
+                child: Text('Save'),
+                onPressed: _handleForm,
+                // textColor: Colors.white,
+              )
+            ],
+            ),
+        ),
+      ),
+    );
+  }
+}
